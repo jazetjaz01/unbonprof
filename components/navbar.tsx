52d6c72e12d196dd6c955d51ae52d1c42aafb564
Alami ERHAIDI <alamierhaidi@iMac-de-Alami-2.local>
2026-09-09 16:39:47 +0200

    update

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,7 +6,7 @@ import { NavigationSheet } from "@/components/navigation-sheet";
 const Navbar = () => {
   return (
     <nav className="h-16 border-b bg-background">
-      <div className="mx-auto flex h-full max-w-(--breakpoint-xl) items-center justify-between px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto flex h-full max-w-(--breakpoint-3xl) items-center justify-between px-4 sm:px-6 lg:px-8">
         <Logo />
 
         {/* Desktop Menu */}
