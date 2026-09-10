294eadfe01514ba5cc672b989e4dba68eb17088d
Alami ERHAIDI <alamierhaidi@iMac-de-Alami-2.local>
2026-09-10 22:09:22 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="flex flex-1 flex-col items-center justify-center gap-8 px-4 text-center">
-      <h1 className="text-6xl font-bold">Trouvez le bon professeur</h1>
+      <h1 className="text-6xl font-bold">Trouvez le <br/>bon professeur</h1>
 
       <form
         action="/recherche"
