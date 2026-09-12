b17102332274652c0b964212112c760d7c6d6f02
Alami ERHAIDI <alamierhaidi@iMac-de-Alami-2.local>
2026-09-12 11:30:14 +0200

    update navbar

--- components/navigation-sheet.tsx
+++ components/navigation-sheet.tsx
@@ -9,7 +9,7 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { Logo } from "@/components/logo";
-import { NavMenu } from "@/components/nav-menu";
+// import { NavMenu } from "@/components/nav-menu";
 
 export const NavigationSheet = () => {
   return (
@@ -19,21 +19,19 @@ export const NavigationSheet = () => {
       </VisuallyHidden>
 
       <SheetTrigger render={<Button size="icon" variant="outline" />}><Menu /></SheetTrigger>
-      <SheetContent className="px-6 py-3">
+      <SheetContent className="flex flex-col px-6 py-3">
         <Logo />
-        <NavMenu className="mt-6 [&>div]:h-full" orientation="vertical" />
+        {/* <NavMenu className="mt-6 [&>div]:h-full" orientation="vertical" /> */}
 
-        <div className="mt-6 flex flex-col gap-2">
+        <div className="flex flex-1 items-center justify-center gap-3">
           <Button
             variant="outline"
-            className="w-full"
             nativeButton={false}
             render={<Link href="/auth/login" />}
           >
             Connexion
           </Button>
           <Button
-            className="w-full"
             nativeButton={false}
             render={<Link href="/auth/sign-up" />}
           >
