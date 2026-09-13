42fbe108b9d3caddd37dd08d0f65d90745b1f9f0
Alami ERHAIDI <alamierhaidi@iMac-de-Alami-2.local>
2026-09-13 20:51:16 +0200

    update dashboard

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = async () => {
 
   return (
     <nav className="h-16  bg-background">
-      <div className="mx-auto flex h-full max-w-(--breakpoint-3xl) items-center justify-between px-4 sm:px-6 lg:px-8 bg-slate-50">
+      <div className="mx-auto flex h-full max-w-(--breakpoint-3xl) items-center justify-between px-4 sm:px-6 lg:px-8 bg-white">
         <Logo />
 
         {/* Desktop Menu 
