259b93eafc80a2b61124e10bcb9865e138965d92
Alami ERHAIDI <alamierhaidi@iMac-de-Alami-2.local>
2026-09-13 21:16:18 +0200

    update dashboard

--- app/dashboard/profil/page.tsx
+++ app/dashboard/profil/page.tsx
@@ -15,7 +15,7 @@ export default async function ProfilPage() {
     .single();
 
   return (
-    <div className="max-w-l border">
+    <div className="max-w-lg border bg-white p-3">
       <div className="mb-4">
         <h1 className="font-heading text-base leading-snug font-medium">Mon profil</h1>
         <p className="text-sm text-muted-foreground">Ces informations sont utilisées sur unbonprof.</p>
