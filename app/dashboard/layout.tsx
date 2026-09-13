d66c003106822ddfd1c5c01dc66b933fb50d73ec
Alami ERHAIDI <alamierhaidi@iMac-de-Alami-2.local>
2026-09-13 20:53:04 +0200

    update dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -22,7 +22,7 @@ export default async function DashboardLayout({
     .single();
 
   return (
-    <div className="flex-1 bg-slate-50">
+    <div className="flex-1 bg-linear-to-t from-orange-50 to-white">
       <div className="mx-auto flex w-full max-w-(--breakpoint-xl) flex-col gap-6 px-4 py-8 sm:px-6 md:flex-row lg:px-8">
         <aside className="w-full shrink-0 md:w-52">
           <DashboardProfileCard />
